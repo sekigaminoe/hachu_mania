56b9f2b5735be7c162fc7b510b0d941a059d4b59
EC2 Default User <ec2-user@ip-172-31-16-124.ap-northeast-1.compute.internal>
2023-10-24 07:47:46 +0000

    [Update]テストデータ挿入

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# coding: utf-8
+
+# 管理者アカウント
 Admin.create!(
-  email: 'admin@admin.com',
-  password: '111111'
+  email: ENV['ADMIN_EMAIL'],
+  password: ENV['ADMIN_PASSWORD']
 )
+
+# ユーザーテストデータ
+eiichi = User.find_or_create_by!(email: "eiichi@example.com") do |user|
+  user.name = "渋沢栄一"
+  user.password = "password"
+  user.profile_image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user1.JPG"), filename:"sample-user1.JPG")
+end
+
+takamori = User.find_or_create_by!(email: "takamori@example.com") do |user|
+  user.name = "西郷隆盛"
+  user.password = "password"
+  user.profile_image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user2.JPG"), filename:"sample-user2.JPG")
+end
+
+taisuke = User.find_or_create_by!(email: "taisuke@example.com") do |user|
+  user.name = "板垣退助"
+  user.password = "password"
+  user.profile_image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user3.JPG"), filename:"sample-user3.JPG")
+end
