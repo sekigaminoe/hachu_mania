4b1ff83a9288c5681810e0a76d90c79d39b76c88
EC2 Default User <ec2-user@ip-172-31-16-124.ap-northeast-1.compute.internal>
2023-10-25 02:15:14 +0000

    投稿詳細のいいね非同期修正

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -6,6 +6,7 @@ class Public::UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @posts = @user.posts.page(params[:page]).per(4)
+    @post = @user.posts
   end
 
   def index
