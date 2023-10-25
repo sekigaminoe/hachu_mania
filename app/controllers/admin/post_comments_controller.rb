7ad2eb9f48fb51bd4ba16e9025cf4de83a72df28
EC2 Default User <ec2-user@ip-172-31-16-124.ap-northeast-1.compute.internal>
2023-10-25 01:12:29 +0000

    コメント削除機能修正

--- app/controllers/admin/post_comments_controller.rb
+++ app/controllers/admin/post_comments_controller.rb
@@ -4,5 +4,6 @@ class Admin::PostCommentsController < ApplicationController
   def destroy
     @comment = PostComment.find(params[:id])
     @comment.destroy
+    redirect_to request.referer
   end
 end
